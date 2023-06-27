5c2e934352eddd5b5a810b5f79c3243cfc592670
choiseyeon <choi20014830@gmail.com>
2023-06-28 04:36:46 +0900

    feat: 엔드포인트 추가

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"backend/database"
+	"backend/router"
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 	"log"
 )
 
+func setUpPosts(app *fiber.App) {
+	var path = "/post"
+
+	//app.Get(path, router.AllPosts)
+	//app.Post(path, router.AddPost)
+	app.Get(path+"/:id", router.GetPost)
+	//app.Put("/update", router.PostUpdate)
+	//app.Delete("/delete", router.PostDelete)
+}
+
 func main() {
 	// Connect with database
 	database.ConnectDb()
 	app := fiber.New()
+
+	setUpPosts(app)
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("Pingpong by fiber\n")
