4b72f9314d751be9c9d3a75d7a0f79e182cf2c9f
choiseyeon <choi20014830@gmail.com>
2023-06-28 04:36:00 +0900

    feat: ID를 바탕으로 조회

--- backend/model/model.go
+++ backend/model/model.go
@@ -1,18 +1,10 @@
 package model
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
-
 type Post struct {
-	gorm.Model
-
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	ImageUrl  string    `json:"imageUrl"`
-	State     bool      `json:"state"`
-	IsDeleted bool      `json:"isDeleted"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	ImageUrl  string `json:"imageUrl"`
+	State     bool   `json:"state"`
+	IsDeleted bool   `json:"isDeleted"`
 }
