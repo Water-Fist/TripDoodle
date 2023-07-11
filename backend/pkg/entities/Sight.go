20c71f5a04468a03c7dbd918ac80c18d1995cb22
choiseyeon <choi20014830@gmail.com>
2023-07-12 05:27:45 +0900

    feat: 엔티티 추가

--- backend/pkg/entities/Sight.go
+++ backend/pkg/entities/Sight.go
@@ -2,18 +2,14 @@ package entities
 
 import "time"
 
-type Post struct {
+type Sight struct {
 	ID        int       `json:"id"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	ImageUrl  string    `json:"imageUrl"`
-	State     bool      `json:"state"`
+	Name      string    `json:"title"`
+	Latitude  string    `json:"content"`
+	Longitude string    `json:"imageUrl"`
+	Area      bool      `json:"state"`
 	IsDeleted bool      `json:"isDeleted"`
 	DeletedAt time.Time `json:"deletedAt"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-}
-
-type DeleteRequest struct {
-	ID string `json:"id"`
 }
