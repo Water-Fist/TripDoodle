b10130be9f113b902b49ba98853086fe107d2919
choiseyeon <choi20014830@gmail.com>
2023-07-20 18:37:59 +0900

    feat: 라우터 추가

---
+++ backend/api/routes/sight.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	handler "backend/api/handlers"
+	"backend/pkg/sight"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SightRouter(app fiber.Router, service sight.Service) {
+	app.Get("/sights", handler.GetSights(service))
+	app.Post("/sights", handler.AddSight(service))
+	app.Put("/sights", handler.UpdateSight(service))
+	app.Delete("/sights", handler.RemoveSight(service))
+}
