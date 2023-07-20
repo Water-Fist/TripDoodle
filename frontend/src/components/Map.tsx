313a5a3f6e30a7cd2f0856103b99ad7a77f8fd22
choiseyeon <choi20014830@gmail.com>
2023-07-21 04:21:42 +0900

    feat: 현재 위치 찾기

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,24 +1,28 @@
-import { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import UseLocation from './UseLocation';
 
 declare var kakao: any;
 
 export const MyComponent = () => {
   const mapRef = useRef<HTMLElement | null>(null);
+  const location = UseLocation();
 
   const initMap = () => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(37.483034, 126.902435),
-      level: 2
-    };
+    if (typeof location !== 'string' && location) {
+      const container = document.getElementById('map');
+      const options = {
+        center: new kakao.maps.LatLng(location.latitude, location.longitude),
+        level: 2
+      };
 
-    const map = new kakao.maps.Map(container as HTMLElement, options);
-    (mapRef as MutableRefObject<any>).current = map;
+      const map = new kakao.maps.Map(container as HTMLElement, options);
+      (mapRef as MutableRefObject<any>).current = map;
+    }
   };
 
   useEffect(() => {
     kakao.maps.load(() => initMap());
-  }, [mapRef]);
+  }, [mapRef, location]);
 
   return (
       <div id="map" style={{ width: "500px", height: "400px" }}></div>
---
+++ frontend/src/components/UseLocation.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+const UseLocation = () => {
+    const [location, setLocation] = useState<{latitude: number; longitude: number} | string>('');
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+
+        function success(position: GeolocationPosition) {
+            setLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+        }
+
+        function error() {
+            setLocation({
+                latitude: 37.483034,
+                longitude: 126.902435
+            });
+            console.log("위치 받기 실패");
+        }
+    }, []);
+
+    return location;
+};
+
+export default UseLocation;
