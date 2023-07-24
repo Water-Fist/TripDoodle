9636cbfd57c4094dfb50b34e40807d55eff3c481
choiseyeon <choi20014830@gmail.com>
2023-07-24 23:59:09 +0900

    fix: 쿼리문 수정

--- backend/api/presenter/response/sight.go
+++ backend/api/presenter/response/sight.go
@@ -6,21 +6,34 @@ import (
 )
 
 type Sight struct {
-	ID        int     `json:"id"`
-	Name      string  `json:"title"`
-	Latitude  float32 `json:"latitude"`
-	Longitude float32 `json:"longitude"`
-	Area      bool    `json:"area"`
+	ID                 int     `json:"id"`
+	Name               string  `json:"name"`
+	Type               string  `json:"type"`
+	Province           string  `json:"province"`
+	CityCountyDistrict string  `json:"cityCountyDistrict"`
+	LegalDong          string  `json:"legalDong"`
+	Ri                 string  `json:"ri"`
+	StreetNumber       string  `json:"streetNumber"`
+	BuildingNumber     string  `json:"buildingNumber"`
+	Latitude           float32 `json:"latitude"`
+	Longitude          float32 `json:"longitude"`
 }
 
 func SightSuccessResponse(data *entities.Sight) *fiber.Map {
 	sight := Sight{
-		ID:        data.ID,
-		Name:      data.Name,
-		Latitude:  data.Latitude,
-		Longitude: data.Longitude,
-		Area:      data.Area,
+		ID:                 data.ID,
+		Name:               data.Name,
+		Latitude:           data.Latitude,
+		Longitude:          data.Longitude,
+		Type:               data.Type,
+		Province:           data.Province,
+		CityCountyDistrict: data.CityCountyDistrict,
+		LegalDong:          data.LegalDong,
+		Ri:                 data.Ri,
+		StreetNumber:       data.StreetNumber,
+		BuildingNumber:     data.BuildingNumber,
 	}
+
 	return &fiber.Map{
 		"state": true,
 		"data":  sight,
