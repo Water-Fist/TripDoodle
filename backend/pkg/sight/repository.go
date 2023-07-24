6fc5e6835a902f22fdd6cb9d2e33236116edb43c
choiseyeon <choi20014830@gmail.com>
2023-07-24 23:59:09 +0900

    fix: 쿼리문 수정

--- backend/pkg/sight/repository.go
+++ backend/pkg/sight/repository.go
@@ -28,18 +28,18 @@ func NewRepo(Db *sql.DB) Repository {
 func (r *repository) CreateSight(sight *entities.Sight) (*entities.Sight, error) {
 	query :=
 		`
-			INSERT INTO 
-			    sights (name, latitude, longitude, area, is_deleted, created_at, updated_at) 
-			VALUES 
-			    ($1, $2, $3, $4, $5, $6, $7) 
-			RETURNING id
-		`
+		INSERT INTO sights 
+		(name, type, province, city_county_district, legal_dong, ri, street_number, building_number, latitude, longitude, is_deleted, created_at, updated_at) 
+		VALUES 
+		($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) 
+		RETURNING id
+   	 	`
 
 	sight.IsDeleted = false
 	sight.CreatedAt = time.Now()
 	sight.UpdatedAt = time.Now()
 
-	err := r.Db.QueryRow(query, sight.Name, sight.Latitude, sight.Longitude, sight.Area, sight.IsDeleted, sight.CreatedAt, sight.UpdatedAt).Scan(&sight.ID)
+	err := r.Db.QueryRow(query, sight.Name, sight.Type, sight.Province, sight.CityCountyDistrict, sight.LegalDong, sight.Ri, sight.StreetNumber, sight.BuildingNumber, sight.Latitude, sight.Longitude, sight.IsDeleted, sight.CreatedAt, sight.UpdatedAt).Scan(&sight.ID)
 	if err != nil {
 		return nil, err
 	}
@@ -54,9 +54,15 @@ func (r *repository) ReadSight() (*[]response.Sight, error) {
 				name, 
 				latitude, 
 				longitude,
-				area
-			FROM 
-			    sights 
+				type, 
+				province, 
+				city_county_district, 
+				legal_dong,
+				ri,
+				street_number, 
+				building_number
+			FROM
+			    sights
 			WHERE 
 			    is_deleted = false
 		`
@@ -70,7 +76,9 @@ func (r *repository) ReadSight() (*[]response.Sight, error) {
 	var sights []response.Sight
 	for rows.Next() {
 		var sight response.Sight
-		err := rows.Scan(&sight.ID, &sight.Name, &sight.Latitude, &sight.Longitude, &sight.Area)
+		err := rows.Scan(&sight.ID, &sight.Name, &sight.Latitude, &sight.Longitude,
+			&sight.Type, &sight.Province, &sight.CityCountyDistrict, &sight.LegalDong,
+			&sight.Ri, &sight.StreetNumber, &sight.BuildingNumber)
 		if err != nil {
 			return nil, err
 		}
@@ -90,19 +98,38 @@ func (r *repository) UpdateSight(sight *entities.Sight) (*entities.Sight, error)
 			UPDATE 
 			    sights 
 			SET 
-			    name = $1,
-			    latitude = $2, 
-			    longitude = $3, 
-			    area = $4, 
-			    is_deleted = $5, 
-			    updated_at = $6 
-			WHERE 
-			    id = $7
+				name = $1, 
+				type = $2, 
+				province = $3, 
+				city_county_district = $4, 
+				legal_dong = $5, 
+				ri = $6, 
+				street_number = $7, 
+				building_number = $8, 
+				latitude = $9, 
+				longitude = $10, 
+				updated_at = $11 
+			WHERE id = $12
 		`
 
 	sight.UpdatedAt = time.Now()
 
-	_, err := r.Db.Exec(query, sight.Name, sight.Latitude, sight.Longitude, sight.Area, sight.IsDeleted, sight.UpdatedAt, sight.ID)
+	_, err := r.Db.Exec(
+		query,
+		sight.Name,
+		sight.Type,
+		sight.Province,
+		sight.CityCountyDistrict,
+		sight.LegalDong,
+		sight.Ri,
+		sight.StreetNumber,
+		sight.BuildingNumber,
+		sight.Latitude,
+		sight.Longitude,
+		sight.UpdatedAt,
+		sight.ID,
+	)
+
 	if err != nil {
 		return nil, err
 	}
@@ -139,8 +166,7 @@ func (r *repository) LoadSight(Latitude float32, Longitude float32) (*[]response
 			id,
 			name,
 			latitude,
-			longitude,
-			area
+			longitude
 		FROM 
 			sights
 		WHERE 
@@ -158,7 +184,7 @@ func (r *repository) LoadSight(Latitude float32, Longitude float32) (*[]response
 	var sights []response.Sight
 	for rows.Next() {
 		var sight response.Sight
-		err := rows.Scan(&sight.ID, &sight.Name, &sight.Latitude, &sight.Longitude, &sight.Area)
+		err := rows.Scan(&sight.ID, &sight.Name, &sight.Latitude, &sight.Longitude)
 		if err != nil {
 			return nil, err
 		}
