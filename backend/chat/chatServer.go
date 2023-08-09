cfcce80651dd9a81c6734023319c69dd17d70a8d
choiseyeon <choi20014830@gmail.com>
2023-08-09 18:44:28 +0900

    feat: 메시지 보내고 받기

--- backend/chat/chatServer.go
+++ backend/chat/chatServer.go
@@ -4,6 +4,7 @@ type WsServer struct {
 	clients    map[*Client]bool
 	register   chan *Client
 	unregister chan *Client
+	broadcast  chan []byte
 }
 
 func NewWebsocketServer() *WsServer {
@@ -14,10 +15,13 @@ func NewWebsocketServer() *WsServer {
 		register: make(chan *Client),
 		// 클라이언트 등록 해제 채널
 		unregister: make(chan *Client),
+		// 브로드캐스트 채널
+		broadcast: make(chan []byte),
 	}
 }
 
 // 각각의 메시지에 따라 해당 클라이언트를 등록하거나 등록 해제하는 작업을 수행합니다.
+// 또한, 브로드캐스트 메시지를 받으면 모든 클라이언트에게 메시지를 전송합니다.
 func (server *WsServer) Run() {
 	for {
 		select {
@@ -27,18 +31,28 @@ func (server *WsServer) Run() {
 		// 클라이언트 등록 해제 채널에서 메시지를 기다립니다.
 		case client := <-server.unregister:
 			server.unregisterClient(client)
+		// 브로드캐스트 채널에서 메시지를 기다립니다.
+		case message := <-server.broadcast:
+			server.broadcastToClients(message)
 		}
 	}
 }
 
-// 주어진 클라이언트를 clients 맵에 추가하는 메서드
+// 주어진 클라이언트를 clients 맵에 추가하는 함수
 func (server *WsServer) registerClient(client *Client) {
 	server.clients[client] = true
 }
 
-// 주어진 클라이언트를 clients 맵에서 제거하는 메서드
+// 주어진 클라이언트를 clients 맵에서 제거하는 함수
 func (server *WsServer) unregisterClient(client *Client) {
 	if _, ok := server.clients[client]; ok {
 		delete(server.clients, client)
+	}
+}
+
+// 주어진 메시지를 모든 클라이언트에게 전송하는 함수
+func (server *WsServer) broadcastToClients(message []byte) {
+	for client := range server.clients {
+		client.send <- message
 	}
 }
--- backend/chat/main.go
+++ backend/chat/main.go
@@ -11,8 +11,11 @@ var addr = flag.String("addr", ":8080", "http server address")
 func main() {
 	flag.Parse()
 
+	wsServer := NewWebsocketServer()
+	go wsServer.Run()
+
 	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		ServeWs(w, r)
+		ServeWs(wsServer, w, r)
 	})
 
 	fs := http.FileServer(http.Dir("./public"))
