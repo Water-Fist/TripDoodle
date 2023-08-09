82f6382f84935644a85e9d3c42eb3cec72b25963
choiseyeon <choi20014830@gmail.com>
2023-08-09 18:50:51 +0900

    feat: 화면단 추가

--- backend/chat/public/assets/app.js
+++ backend/chat/public/assets/app.js
@@ -2,18 +2,38 @@ var app = new Vue({
     el: '#app',
     data: {
         ws: null,
-        serverUrl: "ws://localhost:8080/chat"
+        serverUrl: "ws://localhost:8080/chat",
+        messages: [],
+        newMessage: ""
     },
     mounted: function() {
-        this.connectToWebsocket()
+        this.connectToWebsocket();
     },
     methods: {
         connectToWebsocket() {
             this.ws = new WebSocket( this.serverUrl );
             this.ws.addEventListener('open', (event) => { this.onWebsocketOpen(event) });
+            this.ws.addEventListener('message', (event) => { this.handleNewMessage(event) });
         },
         onWebsocketOpen() {
-            console.log("connected to chat!");
+            console.log("connected to WebSocket!");
+        },
+        handleNewMessage(event) {
+            let data = event.data;
+            data = data.split(/\r?\n/);
+
+            for (let i = 0; i < data.length; i++) {
+                let msg = JSON.parse(data[i]);
+                this.messages.push(msg);
+
+            }
+        },
+        sendMessage() {
+            if(this.newMessage !== "") {
+                this.ws.send(JSON.stringify({message: this.newMessage}));
+                this.newMessage = "";
+            }
         }
+
     }
 })
