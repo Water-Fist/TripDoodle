4fefcfe541cc1441714a223c12d6901b68e98cb3
choiseyeon <choi20014830@gmail.com>
2023-08-17 05:24:42 +0900

    fix: post 서비스단 변경

--- backend/server/pkg/post/service.go
+++ backend/server/pkg/post/service.go
@@ -1,14 +1,15 @@
 package post
 
 import (
+	"server/api/presenter/request"
 	"server/api/presenter/response"
 	"server/pkg/entities"
 )
 
 type Service interface {
-	InsertPost(post *entities.Post) (*entities.Post, error)
+	InsertPost(post *request.PostRequest) (*entities.Post, error)
 	FetchPosts() (*[]response.Post, error)
-	UpdatePost(post *entities.Post) (*entities.Post, error)
+	UpdatePost(post *request.UpdatePostRequest) (*entities.Post, error)
 	RemovePost(ID string) error
 }
 
@@ -22,16 +23,30 @@ func NewService(r Repository) Service {
 	}
 }
 
-func (s *service) InsertPost(post *entities.Post) (*entities.Post, error) {
-	return s.repository.CreatePost(post)
+func (s *service) InsertPost(post *request.PostRequest) (*entities.Post, error) {
+	entityPost := &entities.Post{
+		Title:    post.Title,
+		Content:  post.Content,
+		ImageUrl: post.ImageUrl,
+		State:    post.State,
+		SightId:  post.SightID,
+	}
+	return s.repository.CreatePost(entityPost)
 }
 
 func (s *service) FetchPosts() (*[]response.Post, error) {
 	return s.repository.ReadPost()
 }
 
-func (s *service) UpdatePost(post *entities.Post) (*entities.Post, error) {
-	return s.repository.UpdatePost(post)
+func (s *service) UpdatePost(post *request.UpdatePostRequest) (*entities.Post, error) {
+	entityPost := &entities.Post{
+		ID:       post.ID,
+		Title:    post.Title,
+		Content:  post.Content,
+		ImageUrl: post.ImageUrl,
+		State:    post.State,
+	}
+	return s.repository.UpdatePost(entityPost)
 }
 
 func (s *service) RemovePost(ID string) error {
