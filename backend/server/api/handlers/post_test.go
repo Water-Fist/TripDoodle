cca7017804321cfb7474aa5842108e1af7a6fcc2
choiseyeon <choi20014830@gmail.com>
2023-08-24 17:16:03 +0900

    fix: AddPost 테스트 코드 수정

--- backend/server/api/handlers/post_test.go
+++ backend/server/api/handlers/post_test.go
@@ -17,24 +17,27 @@ type MockPostService struct {
 	mock.Mock
 }
 
-func (m *MockPostService) InsertPost(post *request.PostRequest) (*entities.Post, error) {
-	args := m.Called(post)
+func (m *MockPostService) InsertPost(req *request.PostRequest) (*entities.Post, error) {
+	args := m.Called(req)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
 	return args.Get(0).(*entities.Post), args.Error(1)
 }
 
 func (m *MockPostService) FetchPosts() (*[]response.Post, error) {
-	args := m.Called()
-	return args.Get(0).(*[]response.Post), args.Error(1)
+	//TODO implement me
+	panic("implement me")
 }
 
 func (m *MockPostService) UpdatePost(post *request.UpdatePostRequest) (*entities.Post, error) {
-	args := m.Called(post)
-	return args.Get(0).(*entities.Post), args.Error(1)
+	//TODO implement me
+	panic("implement me")
 }
 
 func (m *MockPostService) RemovePost(ID string) error {
-	args := m.Called(ID)
-	return args.Error(0)
+	//TODO implement me
+	panic("implement me")
 }
 
 func TestAddPost(t *testing.T) {
@@ -45,14 +48,21 @@ func TestAddPost(t *testing.T) {
 
 	t.Run("it should return 400 for invalid request body", func(t *testing.T) {
 		req := httptest.NewRequest("POST", "/posts", bytes.NewBuffer([]byte("invalid_json")))
+		req.Header.Set("Content-Type", "application/json")
 		resp, _ := app.Test(req)
 
 		assert.Equal(t, 400, resp.StatusCode)
 	})
 
 	t.Run("it_should_return_400_if_title_or_content_is_missing", func(t *testing.T) {
-		body := `{"title": "", "content": "some content"}`
+		body := `{
+			  "title": "",
+			  "content": "test",
+			  "imageUrl": "test",
+			  "sightId": 1
+		  }`
 		req := httptest.NewRequest("POST", "/posts", bytes.NewBuffer([]byte(body)))
+		req.Header.Set("Content-Type", "application/json")
 		resp, _ := app.Test(req)
 
 		assert.Equal(t, 400, resp.StatusCode)
@@ -60,28 +70,29 @@ func TestAddPost(t *testing.T) {
 
 	t.Run("it should return 500 for internal server error", func(t *testing.T) {
 		body := `{
-        "title": "ooo",
-        "content": "ㄴㄴ",
-        "imageUrl": "ㄴㄴㄴ",
-        "state": true,
-        "sightID": 1
-    }`
+			  "title": "test",
+			  "content": "test",
+			  "imageUrl": "test",
+			  "sightId": 1
+		  }`
 		req := httptest.NewRequest("POST", "/posts", bytes.NewBuffer([]byte(body)))
-		mockService.On("InsertPost", mock.Anything).Return(nil, errors.New("Some error")).Once()
+		req.Header.Set("Content-Type", "application/json")
+		mockService.On("InsertPost", mock.Anything).Return(nil, errors.New("500 error")).Once()
 		resp, _ := app.Test(req)
 
 		assert.Equal(t, 500, resp.StatusCode)
+		mockService.AssertExpectations(t)
 	})
 
 	t.Run("it should return 200 for successful post insertion", func(t *testing.T) {
 		body := `{
-        "title": "ooo",
-        "content": "ㄴㄴ",
-        "imageUrl": "ㄴㄴㄴ",
-        "state": true,
-        "sightID": 1
-    }`
+			  "title": "test",
+			  "content": "test",
+			  "imageUrl": "test",
+			  "sightId": 1
+		  }`
 		req := httptest.NewRequest("POST", "/posts", bytes.NewBuffer([]byte(body)))
+		req.Header.Set("Content-Type", "application/json")
 		post := entities.Post{
 			ID:       1,
 			Title:    "test title",
--- backend/server/go.mod
+++ backend/server/go.mod
@@ -6,38 +6,36 @@ require (
 	github.com/gofiber/fiber/v2 v2.48.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.8.4
+	github.com/swaggo/fiber-swagger v1.3.0
 	github.com/swaggo/swag v1.16.1
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.20.0 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/spec v0.20.9 // indirect
 	github.com/go-openapi/swag v0.22.4 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/swaggo/fiber-swagger v1.3.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
-	github.com/urfave/cli/v2 v2.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
